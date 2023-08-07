54de0ca795934e7d6bb564e11e38c39c41ab6d2c
jia <jojoalanazi@icloud.com>
2023-08-07 19:00:29 +0300

    done

--- Day1/script.js
+++ Day1/script.js
@@ -1,15 +1,18 @@
+
+//let
 let myage=17;
 let Myname="Aljawharah";
 
-
+//var
 var myname="Aljawharah Alanazi";
 
 
-
+//const for var
 const myname="aljawharah";
-
+//const for let
 const Myname="a"
 const myage="17";
-
-console.log(myname)
-console.log(Myname)
+//print
+console.log(myname);
+console.log(Myname);
+console.log(myage)
