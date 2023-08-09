2c6f4e559f0329c17fb7e0e93e99141e683e45f4
jia <jojoalanazi@icloud.com>
2023-08-09 23:23:19 +0300

    2nd homework with js

---
+++ Day2/script.js
@@ -0,0 +1,63 @@
+//1st TASK
+
+
+//Number To String
+
+let num=42;
+const numTOstr=String(num);
+
+console.log(`${typeof num} ${num}`);
+console.log(`${typeof numTOstr} ${numTOstr}`);
+
+//String To Number
+
+let str="25";
+const strTOnum=Number(str);
+
+
+console.log(`${typeof str} ${str}`);
+console.log(`${typeof strTOnum} ${strTOnum}`);
+
+
+//2nd TASK
+
+//Plus And Minus
+
+let string="10";
+const strTonum=Number(string);
+
+console.log(strTonum+20);
+console.log(strTonum-2);
+
+
+//3ed TASK
+
+//String To Float
+
+let str1="15.5";
+const strTOfloat=Number(str1);
+
+
+console.log(strTOfloat*2);
+console.log(strTOfloat/2);
+
+//4th TASK
+
+//String - Number Plus And Minus
+
+let str3="5";
+let num2=3;
+
+console.log(str3-num2);
+console.log(str3+num2);
+
+//5th TASK
+
+//Boolean And Number
+
+let bool=true;
+let num3=1;
+
+console.log(bool+num3);
+
+//DONE BY ALJAWHARAH//
