e3b6b30417e996f3851f3eb1999e651c8e87c740
jia <jojoalanazi@icloud.com>
2023-08-14 14:26:23 +0300

    Done

---
+++ Day4/script.js
@@ -0,0 +1,15 @@
+let names=["Ali","Samah","Khaled","Fahad","Mohammed"];
+
+console.log(names[2]);
+
+console.log(names.unshift('Aljawharah'));
+console.log(names);
+
+console.log(names.push('Alghalia'));
+console.log(names);
+
+names.shift();
+console.log(names);
+
+names.pop();
+console.log(names);
