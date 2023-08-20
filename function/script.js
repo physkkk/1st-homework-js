c1bf45a4e84ed611ee25e534dda12e758fc87fdb
jia <jojoalanazi@icloud.com>
2023-08-20 20:48:42 +0300

    done

---
+++ function/script.js
@@ -0,0 +1,32 @@
+//Homework
+
+function javascript_Boot_camp(teach) {
+    teach="This javaScript BootCamp with "
+
+    let firstandsec=['Teacher1','Teacher2']; 
+    console.log(teach + firstandsec[0]+' ok?');
+    console.log(teach + firstandsec[1]+' ok?');
+}
+javascript_Boot_camp('ok')
+//2nd task
+
+let arr =(c,r)=>{
+
+    let result= c + r ;
+    return result/2
+
+ }
+ 
+ console.log(arr(7,5));
+ 
+ 
+
+ let ar =(b,d)=>{
+
+    let result= b + d ;
+    return result/2
+
+ }
+ 
+ console.log(ar(5.6,10.8));
+ 
